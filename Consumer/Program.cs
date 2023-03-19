ccc69f0682fa66869134bed40082c9dc47df8570
hoztekin <hoztekin81@hotmail.com>
2023-03-19 15:41:20 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Bağlantı oluşturma
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.Uri = new("amqps://rwbcodtq:CLgFjQjBrxJqVhDCFTBLel6RFHyqV7nL@shrimp.rmq.cloudamqp.com/rwbcodtq");  //Manager AMQP Details Url 
+
+			//Bağlantıyı Aktifleştirme ve Kanal Açma
+			using IConnection connection = factory.CreateConnection();
+			using IModel channel = connection.CreateModel();
+
+			//Queue Oluşturma
+			channel.QueueDeclare(queue: "example-queue", exclusive: false); //Cunsomerdaki kuyruk publisher ile aynı olmalıdır.
+
+
+			//Queuedaki mesajı okuma
+			EventingBasicConsumer consumer = new(channel);
+			channel.BasicConsume(queue: "example-queue", false, consumer ); //AutoAck mesaj okunduktan sonra kuyruktan silinip silinmemesi için kullanılan parametredir.
+			consumer.Received += (sender, e) =>
+			{
+				//Kuyruğa gelen mesajın işlendiği yer
+				//e.body: Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+				//e.Body.Span veya e.body.ToArray() kuyruktaki mesajı byte olarak bize verecektir.
+				Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+			};
+			Console.ReadLine();
+		}
+	}
+}  
---
+++ Publisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Rabbit Mq Erland diliyle geliştirilmiştir.
+			 * Open source olan bir mesaj quering sistemdir.
+			 * Cross platformdur.
+			 * Default exchange direct exchange'dir.
+			 * Dağıtık yapılarda fanout exchange kullanılabilir.
+			 * Direct exchange'in default router ismi kuyruk ismidir.
+			 */
+			
+
+			//Bağlantı oluşturma
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.Uri = new("amqps://rwbcodtq:CLgFjQjBrxJqVhDCFTBLel6RFHyqV7nL@shrimp.rmq.cloudamqp.com/rwbcodtq");  //Manager AMQP Details Url 
+
+
+			//Bağlantıyı Aktifleştirme ve Kanal Açma
+			using IConnection connection = factory.CreateConnection();
+			using IModel channel =  connection.CreateModel();
+
+
+			//Queue Oluşturma
+			channel.QueueDeclare(queue: "example-queue", exclusive:false);
+
+			//Queue'ya mesaj gönderme
+
+			//RabbitMQ kuyruğa atacağı mesajları byte türünden göndermektedir. Dolayısıyla mesajşarımızı byte a dönüştürmemiz gerekir.
+
+			byte[] message = Encoding.UTF8.GetBytes("Test Mesajı");
+			channel.BasicPublish(exchange:"", routingKey: "example-queue", body:message);
+
+			Console.ReadLine();
+
+		}
+	}
+}
